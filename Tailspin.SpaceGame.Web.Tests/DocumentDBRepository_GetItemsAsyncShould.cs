83c93d004c008cc0edab0c13df694fbc3b04fc5d
Adam Grocholski <adam_grocholski@hotmail.com>
2020-07-27 16:53:47 -0500

    migrated code for #1

---
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TailSpin.SpaceGame.Web.Models;
+
+namespace Tests
+{
+    public class DocumentDBRepository_GetItemsAsyncShould
+    {
+        [TestCase("Milky Way")]
+        [TestCase("Andromeda")]
+        [TestCase("Pinwheel")]
+        [TestCase("NGC 1300")]
+        [TestCase("Messier 82")]
+        public void FetchOnlyRequestedGameRegion(string gameRegion)
+        {
+            var expected = gameRegion;
+            var actual = gameRegion;
+
+            Assert.IsTrue(expected == actual);
+        }
+    }
+}
